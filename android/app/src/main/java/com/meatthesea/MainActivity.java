5ad58d0ae0f6cdbf2e1ef2bcfb54252abbd651af
= <=>
2020-11-04 22:54:09 +0800

    update

--- android/app/src/main/java/com/meatthesea/MainActivity.java
+++ android/app/src/main/java/com/meatthesea/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mts;
+package com.meatthesea;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "MTS";
+    return "MeatTheSea";
   }
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
--- ios/MeatTheSeaTests/MeatTheSeaTests.m
+++ ios/MeatTheSeaTests/MeatTheSeaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MTSTests : XCTestCase
+@interface MeatTheSeaTests : XCTestCase
 
 @end
 
-@implementation MTSTests
+@implementation MeatTheSeaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
